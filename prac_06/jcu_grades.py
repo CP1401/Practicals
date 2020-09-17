159d90446884daaea2a9d549cf1e9c2b5118c464
Lindsay Ward <lindsay.ward@jcu.edu.au>
2020-09-17 16:25:34 +1000

    Add selected prac 6 solutions

---
+++ prac_06/jcu_grades.py
@@ -0,0 +1,39 @@
+"""
+CP1401 Practical 06 - Functions 1 - Suggested Solution
+JCU Grades
+"""
+
+import random
+
+
+def main():
+    """Display JCU grades from user scores and random ones."""
+    score = float(input("Score: "))
+    while score >= 0:
+        grade = determine_grade(score)
+        print(f"{score} = {grade}")
+        score = float(input("Score: "))
+
+    number_of_scores = int(input("How many random scores: "))
+    for i in range(number_of_scores):
+        score = random.randint(0, 100)
+        grade = determine_grade(score)
+        # Notice that the printing of score = is done here, not in the function
+        # It is not the function's job (SRP) to know how the grade will be displayed!
+        print(f"{score} = {grade}")
+
+
+def determine_grade(score):
+    """Determine JCU grade based on score."""
+    if score < 50:
+        return "N"
+    elif score < 65:
+        return "P"
+    elif score < 75:
+        return "C"
+    elif score < 85:
+        return "D"
+    return "HD"
+
+
+main()
---
+++ prac_06/programs.py
@@ -0,0 +1,58 @@
+"""
+CP1401 Practical 06 - Functions 1 - Suggested Solution
+Note: As always, your learning is the goal,
+so learn from the solutions by comparing them to your finished work.
+"""
+
+
+# 1. Coffee Brew Ratio
+
+def main():
+    """Get user's coffee details and display what kind of brew it is."""
+    dose = get_valid_number("Dose: ", 0, 100)
+    coffee_yield = get_valid_number("Yield: ", 0, 200)
+    ratio = coffee_yield / dose
+    style = determine_coffee_style(ratio)
+    print(f"1:{ratio} is considered {style}")
+
+
+def determine_coffee_style(ratio):
+    """Determine the style of coffee based on the brew ratio."""
+    if ratio < 2:
+        return "ristretto"
+    elif ratio < 3:
+        return "normale"
+    else:
+        return "lungo"
+
+
+def get_valid_number(prompt, low, high):
+    """Get a valid number between low and high inclusive."""
+    number = float(input(prompt))
+    while number < low or number > high:
+        print("Invalid input")
+        number = float(input(prompt))
+    return number
+
+
+def test_coffee():
+    """Run quick tests to ensure determine_coffee_style works as expected."""
+    style = determine_coffee_style(1)
+    print(style)  # This should be ristretto
+    style = determine_coffee_style(2)
+    print(style)  # This should be normale
+    style = determine_coffee_style(3.5)
+    print(style)  # This should be lungo
+
+
+def test_coffee_loop():
+    """Run through a number of brew ratios, printing the style for each."""
+    for i in range(0, 5):
+        ratio = i + 0.5
+        style = determine_coffee_style(ratio)
+        print(f"1:{ratio} is considered {style}")
+
+
+# test_coffee()
+# test_coffee_loop()
+main()
