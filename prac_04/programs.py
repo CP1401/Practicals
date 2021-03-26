78185951d426dda269850efd33c0095ff0ad4f48
Lindsay Ward <lindsay.ward@jcu.edu.au>
2021-03-26 21:55:14 +1000

    Add selected prac 4 solutions

---
+++ prac_04/programs.py
@@ -0,0 +1,40 @@
+"""
+CP1401 Practical 04 - Repetition Structures - Suggested Solution
+Please remember to use these solutions responsibly
+- to enhance your learning, not to avoid learning.
+If you get stuck on something, go back to the teaching and work hard to solve the problem.
+"""
+
+# 3. Write a program that prints smiley and frowny faces until the user quits.
+# The user should see a menu like this: (S)miley (F)rowny (Q)uit
+# Make the program print a smiley face (if they press 's'),
+# a frowny face, or an error message ("Invalid choice") depending on their choice.
+# When the user quits, print a farewell message.
+
+print("(S)miley (F)rowny (Q)uit")
+choice = input("> ").upper()
+while choice != "Q":
+    if choice == "S":
+        print(":)")
+    elif choice == "F":
+        print(":(")
+    else:
+        print("Invalid choice")
+    print("(S)miley (F)rowny (Q)uit")
+    choice = input("> ").upper()
+print("Farewell")
+
+# 6. Nested Loops
+# Write a program that asks the user for a number of rows and columns, then prints the numbers to mach, like:
+# Rows: 3
+# Columns: 7
+# 0 1 2 3 4 5 6
+# 0 1 2 3 4 5 6
+# 0 1 2 3 4 5 6
+
+rows = int(input("Rows: "))
+columns = int(input("Columns: "))
+for i in range(rows):
+    for j in range(columns):
+        print(j, end=" ")
+    print()
