535be8d6353b367778b1411750c42e3255b16695
Lindsay Ward <lindsay.ward@jcu.edu.au>
2025-05-06 14:47:56 +1000

    Simplify dog years solution

--- prac_07/dog_years.py
+++ prac_07/dog_years.py
@@ -2,19 +2,19 @@
 
 
 def main():
-    """Calculate dog years for human years inputs."""
+    """Program to convert human years to dog years from user inputs."""
     age_in_human_years = int(input("Age in human years: "))
     while age_in_human_years >= 0:
-        age_in_dog_years = calculate_dog_years(age_in_human_years)
-        print("Age in dog years is", age_in_dog_years)
+        print("Age in dog years is", calculate_dog_years(age_in_human_years))
         age_in_human_years = int(input("Age in human years: "))
     print(":)")
 
 
 def calculate_dog_years(age_in_human_years):
-    """Calculate the age of a dog in 'dog years' based on actual 'human' years."""
+    """Calculate the age of a dog in dog years based on actual human years."""
     if age_in_human_years <= 2:
-        age_in_dog_years = age_in_human_years * 10.5
-    else:
-        age_in_dog_years = 21 + (age_in_human_years - 2) * 4
-    return age_in_dog_years
+        return age_in_human_years * 10.5
+    return 21 + (age_in_human_years - 2) * 4
+
+
+main()
